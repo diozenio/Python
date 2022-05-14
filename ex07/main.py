b6d2e0f7f97c2fa23d497498d2c3c4c11b12eaa0
diozenio <d.dionisiobsl@gmail.com>
2022-05-14 20:37:03 -0300

    Maior numero

---
+++ ex07/main.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite o primeiro número: \n"))
+num2 = float(input("Digite o segundo número: \n"))
+num3 = float(input("Digite o terceiro número: \n"))
+
+numeros = [num1, num2, num3]
+maiores = numeros.sort()
+print(f"O maior número é {numeros[2]}")
