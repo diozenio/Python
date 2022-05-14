39654db0c2c4ffc8684d360f60169ed2643fd8a1
diozenio <d.dionisiobsl@gmail.com>
2022-05-14 20:51:18 -0300

    Ordem decrescente

---
+++ ex10/main.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite o primeiro número: \n"))
+num2 = float(input("Digite o segundo número: \n"))
+num3 = float(input("Digite o terceiro número: \n"))
+
+numeros = [num1, num2, num3]
+numeros.sort()
+numeros.reverse()
+print(numeros)
