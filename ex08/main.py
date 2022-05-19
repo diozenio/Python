491a8247504ba7f4f66d07ee3c561d9aa3128443
diozenio <d.dionisiobsl@gmail.com>
2022-05-18 23:33:02 -0300

    Produto mais barato

--- ex08/main.py
+++ ex08/main.py
@@ -1,8 +1,7 @@
-num1 = float(input("Digite o primeiro número: \n"))
-num2 = float(input("Digite o segundo número: \n"))
-num3 = float(input("Digite o terceiro número: \n"))
+num1 = float(input("Digite o primeiro preço: \n"))
+num2 = float(input("Digite o segundo preço: \n"))
+num3 = float(input("Digite o terceiro preço: \n"))
 
 numeros = [num1, num2, num3]
 numeros.sort()
-print(f"O maior número é {numeros[len(numeros)-1]}")
-print(f"O menor número é {numeros[0]}")
+print(f"O melhor produto é o que custa R${numeros[0]}0 reais")
