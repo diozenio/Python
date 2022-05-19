0e5f2b029304631e7e791a36ec16d42e59c45a43
diozenio <d.dionisiobsl@gmail.com>
2022-05-18 23:33:17 -0300

    Turnos

--- ex10/main.py
+++ ex10/main.py
@@ -1,8 +1,12 @@
-num1 = float(input("Digite o primeiro número: \n"))
-num2 = float(input("Digite o segundo número: \n"))
-num3 = float(input("Digite o terceiro número: \n"))
+print("Digite seu turno:")
+print("M-matutino\nV-Vespertino\nN- Noturno.")
+turno = input()
 
-numeros = [num1, num2, num3]
-numeros.sort()
-numeros.reverse()
-print(numeros)
+if turno.upper() == "M":
+    print("Bom dia!")
+elif turno.upper() == "V":
+    print("Boa tarde!")
+elif turno.upper() == "N":
+    print("Boa noite!")
+else:
+    print("Valor Inválido!")
