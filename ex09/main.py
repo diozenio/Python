9f289bec31313a17fe292801316cd99c56050ab6
diozenio <d.dionisiobsl@gmail.com>
2022-05-18 23:33:11 -0300

    Ordem decrescente

--- ex09/main.py
+++ ex09/main.py
@@ -1,7 +1,8 @@
-num1 = float(input("Digite o primeiro preço: \n"))
-num2 = float(input("Digite o segundo preço: \n"))
-num3 = float(input("Digite o terceiro preço: \n"))
+num1 = float(input("Digite o primeiro número: \n"))
+num2 = float(input("Digite o segundo número: \n"))
+num3 = float(input("Digite o terceiro número: \n"))
 
 numeros = [num1, num2, num3]
 numeros.sort()
-print(f"O melhor produto é o que custa R${numeros[0]}0 reais")
+numeros.reverse()
+print(numeros)
