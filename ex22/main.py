0345876257243f4dfa4353dd0e38618b994a35d5
diozenio <68879723+diozenio@users.noreply.github.com>
2022-06-09 07:37:27 -0300

    Numeros pares e impares

---
+++ ex22/main.py
@@ -0,0 +1,7 @@
+while(1==1):
+    numero = int(input('Digite o número: '))
+
+    if(numero % 2 == 0):
+        print("O número {} é par.".format(numero))
+    else:
+        print("O número {} é ímpar.".format(numero))
